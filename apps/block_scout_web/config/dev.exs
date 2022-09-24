367bc8464e7734991c6634e8c1d43c347a8b1bee
Toufiq Elahy <toufiqelahy@hotmail.com>
2022-09-24 08:26:27 +0200

    check

--- apps/block_scout_web/config/dev.exs
+++ apps/block_scout_web/config/dev.exs
@@ -50,14 +50,6 @@ config :block_scout_web, BlockScoutWeb.Endpoint,
 
 config :block_scout_web, BlockScoutWeb.Tracer, env: "dev", disabled?: true
 
-config :logger, :block_scout_web,
-  level: :debug,
-  path: Path.absname("logs/dev/block_scout_web.log")
-
-config :logger, :api,
-  level: :debug,
-  path: Path.absname("logs/dev/api.log"),
-  metadata_filter: [fetcher: :api]
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
--- apps/ethereum_jsonrpc/config/dev.exs
+++ apps/ethereum_jsonrpc/config/dev.exs
@@ -2,6 +2,3 @@ import Config
 
 config :ethereum_jsonrpc, EthereumJSONRPC.Tracer, env: "dev", disabled?: true
 
-config :logger, :ethereum_jsonrpc,
-  level: :debug,
-  path: Path.absname("logs/dev/ethereum_jsonrpc.log")
